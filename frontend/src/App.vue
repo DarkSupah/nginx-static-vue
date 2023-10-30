28c3bd9ca99a8699001ae3c1d85cd96351426b34
Ivan Grachyov <igrachev@smart-ts.ru>
2023-10-30 19:11:08 +0500

    cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,19 +2,4 @@
   <div class="app">
     Test Vue app
   </div>
-  <script type="application/javascript" :src="scriptSrc"></script>
 </template>
-
-<script>
-export default {
-  name: 'App',
-  setup() {
-    const randomHash = Math.random()
-    const scriptSrc = `?hash=${randomHash}`
-
-    return {
-      scriptSrc
-    }
-  }
-}
-</script>
